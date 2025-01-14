e76e5822bcf8dd6e382d53eb3f040e6d93853abf
Joel804 <joelcpaul02@gmail.com>
2025-01-14 10:36:46 -0500

    add elements array

--- week02/lab2.py
+++ week02/lab2.py
@@ -1,3 +1,18 @@
-elements =["Hydrogen","Helium","Lithium","Beryllium","Boron","Carbon"]
-print("Elements",elements)
-# git add . && git commit -m "add elements array" && git push
+elements = ["Hydrogen", "Helium", "Lithium", "Beryllium", "Boron", "carbon"]
+print("Elements: ", elements)
+#  git add . && git commit -m "add elements array" && git push
+
+# def funct_name():
+#     return True
+# def say_greeting(name, message="hi"):
+#     print(f" {message}, {name}")
+# say_greeting("Joel")
+# say_greeting("joel", "Hello")
+
+def get_valid_input(prompt):
+    while True:
+        try:
+            return int(input(print))
+        except ValueError:
+            print("Error: Please enter a valid integer!")
+            continue
